03afca6567e4199fc344923555ba9c7e2387dabb
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-06-23 01:38:40 -0300

    Funciones y Trabajo con DOM

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del número Secreto";
+let numeroSecreto = generarNumeroSecreto();
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un número del 1 al 10";
 
-function intentoDeUsuario(){
-    alert('Click desde el botón')
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+
+function verficarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+    console.log(typeof(numeroDeUsuario))
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto))
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto);
+
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+    
+}
+
+asignarTextoElemento('h1', 'Juego del número Secreto');
+asignarTextoElemento('p', 'Indica un número del 1 al 10')
+
