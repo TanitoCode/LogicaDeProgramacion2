d899ed2816a4ceb1818f8efee9ebe527099c2ddc
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-06-25 00:56:35 -0300

    Tercera parte Reiniciando el juego

--- DesafioCurso2/app.js
+++ DesafioCurso2/app.js
@@ -1,23 +1,23 @@
 let titulo = document.querySelector('h1');
 titulo.innerHTML = "Hora del Desafío";
 
-function clickBoton(){
+function clickBoton() {
     console.log("El boton fue clickado");
 }
 
-function preguntaCiudad(){
+function preguntaCiudad() {
     let ciudad = prompt("Nombra una ciudad de Argentina")
     alert(`Estuve en ${ciudad} y me acorde de ti`)
 }
 
-function alerta(){
+function alerta() {
     alert('Yo amo JS')
 }
 
-function suma(){
+function suma() {
     let num1 = parseInt(prompt("Ingrese un numero"));
     let num2 = parseInt(prompt("Ingrese otro numero"));
-    alert(`La suma de ${num1} mas el ${num2} es ${num1+num2}`)
+    alert(`La suma de ${num1} mas el ${num2} es ${num1 + num2}`)
 }
 
 /*
@@ -30,40 +30,42 @@ Desafíos:
 6-Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
 */
 
-function holaMundo() {
-    console.log('Hola, mundo')
-}
-holaMundo();
+// function holaMundo() {
+//     console.log('Hola, mundo')
+// }
+// holaMundo();
 
-function recibeNombre(nombre) {
-    console.log(`Hola, ${nombre}`)
-}
-recibeNombre('Cristian');
+// function recibeNombre(nombre) {
+//     console.log(`Hola, ${nombre}`)
+// }
+// recibeNombre('Cristian');
 
-function numeroDoble(numero) {
-    let numDoble = numero*2;
-    return console.log(numDoble);
-}
-numeroDoble(7);
+// function numeroDoble(numero) {
+//     let numDoble = numero*2;
+//     return console.log(numDoble);
+// }
+// numeroDoble(7);
 
-function promedioNumeros(num1,num2,num3){
-    let promedio = (num1+num2+num3)/3;
-    return console.log(promedio);
-}
-promedioNumeros(50,90,85);
+// function promedioNumeros(num1,num2,num3){
+//     let promedio = (num1+num2+num3)/3;
+//     return console.log(promedio);
+// }
+// promedioNumeros(50,90,85);
 
-function mayorMenor(num1,num2){
-    if(num1>num2){
-        console.log(num1);
-    }else{
-        console.log(num2);
-    }
-}
-mayorMenor(45,25);
+// function mayorMenor(num1,num2){
+//     if(num1>num2){
+//         console.log(num1);
+//     }else{
+//         console.log(num2);
+//     }
+// }
+// mayorMenor(45,25);
 
-function numeroMultiplicado(numero) {
-    let resultado = numero*numero;
-   return console.log(resultado);
-}
+// function numeroMultiplicado(numero) {
+//     let resultado = numero*numero;
+//    return console.log(resultado);
+// }
 
-numeroMultiplicado(6);
+// numeroMultiplicado(6);
+
+
--- app.js
+++ app.js
@@ -1,30 +1,75 @@
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto = 0;
+let intentos = 0;
 
 
-
-function asignarTextoElemento(elemento, texto){
+function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
 
 
-function verficarIntento(){
+function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
-    console.log(typeof(numeroDeUsuario))
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto))
-    console.log(numeroDeUsuario);
-    console.log(numeroDeUsuario === numeroSecreto);
 
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`)
+        document.getElementById('reiniciar').removeAttribute('disabled')
+       
+        //console.log(intentos)
+    } else {
+        //El usuario no acerto
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor')
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor')
+
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    //console.log(numeroDeUsuario === numeroSecreto);
+    // console.log(typeof(numeroDeUsuario))
+    //console.log(numeroSecreto);
+    // console.log(typeof(numeroSecreto))
+    // console.log(numeroDeUsuario);
     return;
 }
 
-function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;
-    
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+
 }
 
-asignarTextoElemento('h1', 'Juego del número Secreto');
-asignarTextoElemento('p', 'Indica un número del 1 al 10')
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
 
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número Secreto');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10')
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    //Limpiar juego
+    limpiarCaja();
+    //Indicar mensaje de intervalo de numeros
+    //Generar numero aleatorio
+    //inicializar el numero de intentos
+    condicionesIniciales();
+   //Deshabilitar el boton de nuevo juego
+   document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
+
+// function generarNumeroAleatorio(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   }
+
+//   let numeroAleatorio = generarNumeroAleatorio(1, 100);
+//   console.log("El número aleatorio generado es:", numeroAleatorio);
+
+condicionesIniciales();
