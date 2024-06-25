c233ba5a15158de6cf4c97e3d2a411792326854a
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-06-25 20:42:47 -0300

    Lista-Array

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
-
+let listaNumerosSorteados = []
+let numeroMaximo = 10;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -16,7 +17,7 @@ function verificarIntento() {
     if (numeroDeUsuario === numeroSecreto) {
         asignarTextoElemento('p', `Acertaste el número ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`)
         document.getElementById('reiniciar').removeAttribute('disabled')
-       
+
         //console.log(intentos)
     } else {
         //El usuario no acerto
@@ -43,13 +44,27 @@ function limpiarCaja() {
 }
 
 function generarNumeroSecreto() {
-    return Math.floor(Math.random() * 10) + 1;
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
 
+    //Si ya sorteamos todos los numeros
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles')
+    } else {
+        //Si el numero generado esta incluido en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
 }
 
 function condicionesIniciales() {
     asignarTextoElemento('h1', 'Juego del número Secreto');
-    asignarTextoElemento('p', 'Indica un número del 1 al 10')
+    asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`)
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
@@ -61,8 +76,8 @@ function reiniciarJuego() {
     //Generar numero aleatorio
     //inicializar el numero de intentos
     condicionesIniciales();
-   //Deshabilitar el boton de nuevo juego
-   document.getElementById('reiniciar').setAttribute('disabled', 'true');
+    //Deshabilitar el boton de nuevo juego
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
 }
 
 // function generarNumeroAleatorio(min, max) {
